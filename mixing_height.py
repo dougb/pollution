699cafd3e599b04afbae3816bef3a5f6671ccc2f
Doug Balog <doug@balog.net>
2017-08-05 20:45:16 -0400

    updates

--- mixing_height.py
+++ mixing_height.py
@@ -16,7 +16,11 @@ month_map = { "Janurary":1, "Feburary":2, "March":3,
            "April":4, "May":5, "June":6,"July":7,"August":8,
            "September":9, "October":10, "November":11, "December":12 
            }
+
 months_regex = "|".join(month_map.keys())
+
+short_month_map = { "Jan":1,"Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6, "Jul":7,"Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12 }
+short_months_regex = "|".join(short_month_map.keys())
 
 mixing_height_url = "http://forecast.weather.gov/MapClick.php?w0=t&w3=sfcwind&w10=mhgt&AheadHour=0&Submit=Submit&&FcstType=graphical&site=all&menu=1&textField1=%f&textField2=%f"
 
@@ -61,33 +65,47 @@ def fetchUrl(url,cache_filename):
             f.write(data)
     return data
 
-def computeTime(month_str,day,hour,meridiem):
-    mon = month_map[month_str]
-    if meridiem == "pm" and hour != 12:
-        hour += 12
-    elif meridiem == "am" and hour == 12:
-        hour = 0
-
-    now = time.localtime()
-
-    if now.tm_mon == 12 and mon == 1:
-        year = now.tm_year + 1
+def computeTime(month_str,day,hour,meridiem,minute=0,second=0,year=None):
+    if month_map.has_key(month_str):
+        mon = month_map[month_str]
     else:
-        year = now.tm_year
+        mon = short_month_map[month_str]
 
-    ts = int(time.mktime([year,mon,day,hour,0,0,0,0,0]))
+    if meridiem == "pm" and hour != 12:
+        hour += 11
+    elif meridiem == "am":
+        if hour == 12:
+            hour = 0
+        else:
+            hour -= 1
+
+    if not year:
+        now = time.localtime()
+        if now.tm_mon == 12 and mon == 1:
+            year = now.tm_year + 1
+        else:
+            year = now.tm_year
+
+    ts = int(time.mktime([year,mon,day,hour,minute,second,0,0,0]))
 
     print "ts:%d (%s)" % (ts, time.localtime(ts))
 
     return ts
 
 def getLastUpdate(data):
-    return 0
+    grp = re.findall("Last Update: (\d+)\:(\d\d) (am|pm) (\w\w\w) (\w\w\w) (\d+), (\d+)",data,flags=re.MULTILINE)
+    if grp:
+        print "Group:",grp[0]
+        (hr,min,meridem,tz,mon_str,mday,year) = grp[0]
+        return computeTime(mon_str,int(mday),int(hr),meridem,minute=int(min),year=int(year))
+    else:
+        print "ERROR:Couldn't find Last Updated"
+        return -1
 
 def getElevation(data):
-    grp = re.match("Elev\. (\d+) ft",data)
+    grp = re.findall("Elev. (\d+)",data,flags=re.MULTILINE)
     if grp:
-        return int(grp.group(1))
+        return int(grp[0])
     else:
         print "Error Can't figure out Elevation!"
         return -1
@@ -97,47 +115,49 @@ num_points = 1
 
 grid_points = genGrid(40.148688, -80.332527, 40.717326,-79.596443, num_points)
 
-for i in range(len(grid_points)):
-    lat = grid_points[i]['lat']
-    lon = grid_points[i]['lon']
-    fetch_ts = int(time.time())
-    url = mixing_height_url % (lat,lon)
-    print "Fetching url:%s" % (url)
-    html_data = fetchUrl(url,filename % (i))
-    soup = bs4.BeautifulSoup(html_data,"html.parser")
+with open('data.json', 'aw') as o:
 
-    lastUpdate_ts = getLastUpdate(html_data)
-    print "LastUpdate_ts:%d" % (lastUpdate_ts)
-    elevation_ft = getElevation(html_data)
-    print "Elevation:%d ft" % (elevation_ft)
+    for i in range(len(grid_points)):
+        lat = grid_points[i]['lat']
+        lon = grid_points[i]['lon']
+        fetch_ts = int(time.time())
+        url = mixing_height_url % (lat,lon)
+        print "Fetching url:%s" % (url)
+        html_data = fetchUrl(url,filename % (i))
+        soup = bs4.BeautifulSoup(html_data,"html.parser")
 
-    sys.exit(0)
-    weather_data = []
-    root_data = { "lat":lat, "lon":lon, "fetchTs":fetch_ts,
-                  "lastUpdate_ts":lastUpdate_ts, "elevation":elevation_ft,
-                  "data": weather_data }
+        lastUpdate_ts = getLastUpdate(html_data)
+        print "LastUpdate_ts:%d (%s)" % (lastUpdate_ts,time.ctime(lastUpdate_ts))
+        elevation_ft = getElevation(html_data)
+        print "Elevation:%d ft" % (elevation_ft)
 
-    for mp in soup.find_all("map"):
-        for area in mp.find_all("area"):
-            mouse_over = area.attrs['onmouseover']
-            print "MouseOver:%s" % (mouse_over)
-            match = re.match(".*(%s) (\d+)\D+(\d+)([a|p]m).+Temperature: (\d+) .+Surface Wind: (\w+) (\d+)mph.+Mixing Height: (\d+)ft" % (months_regex), mouse_over )
-            if match:
-                month = match.group(1)
-                day = int(match.group(2))
-                hour = int(match.group(3))
-                meridiem = match.group(4)
-                temp_f = int(match.group(5))
-                temp_c = (float(temp_f) - 32.0) * 5.0/9.0 
-                direction = match.group(6)
-                speed = int(match.group(7))
-                mixing_height = int(match.group(8))
-                fts = computeTime(month,day,hour,meridiem)
-                weather_data.append( { "fts":fts, "direction":direction, "speed":speed, 
-                                       "temp":temp_f, 
-                                       "mixing_ht":mixing_height } )
+        weather_data = []
+        root_data = { "lat":lat, "lon":lon, "fetchTs":fetch_ts,
+                      "lastUpdate_ts":lastUpdate_ts, "elevation":elevation_ft,
+                      "data": weather_data }
 
-    print json.dumps(root_data)
-    time.sleep(1)
+        for mp in soup.find_all("map"):
+            for area in mp.find_all("area"):
+                mouse_over = area.attrs['onmouseover']
+                print "MouseOver:%s" % (mouse_over)
+                match = re.match(".*(%s) (\d+)\D+(\d+)([a|p]m).+Temperature: (\d+) .+Surface Wind: (\w+) (\d+)mph.+Mixing Height: (\d+)ft" % (months_regex), mouse_over )
+                if match:
+                    month = match.group(1)
+                    day = int(match.group(2))
+                    hour = int(match.group(3))
+                    meridiem = match.group(4)
+                    temp_f = int(match.group(5))
+                    temp_c = (float(temp_f) - 32.0) * 5.0/9.0 
+                    direction = match.group(6)
+                    speed = int(match.group(7))
+                    mixing_height = int(match.group(8))
+                    fts = computeTime(month,day,hour,meridiem)
+                    weather_data.append( { "fts":fts, "direction":direction, "speed":speed, 
+                                           "temp":temp_f, 
+                                           "mixing_ht":mixing_height } )
+
+                
+        print >> o, json.dumps(root_data)
+        time.sleep(1)
                 
 
